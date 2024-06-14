2c3eccf80a953adb4a1785a37741a1d2df0f18db
James Moore <jamesforee4@gmail.com>
2024-06-14 16:45:22 -0400

    ...

---
+++ ch3/354/main.c
@@ -0,0 +1,5 @@
+
+double funct2(double w, int x, float y, long z)
+{
+  return y * x - w / z;
+}
---
+++ ch3/357/main.c
@@ -0,0 +1,10 @@
+
+double funct3(int* ap, double b, long c, float* dp)
+{
+  if (b <= *ap)
+    return *dp * 2 + c;
+  else
+    return *dp * c;
+}
+
+
