a1f5e2d4b5b1cb148ca527004c7f897aa6eccd69
James Moore <jamesforee4@gmail.com>
2024-09-05 17:58:32 -0400

    ...

--- ch12/1234/matrix.c
+++ ch12/1234/matrix.c
@@ -8,89 +8,112 @@
 
 typedef struct
 {
-  const int** mat1;
-  const int** mat2;
-  int** dest;
+  int** lop;
+  int** rop;
+  int** result;
   size_t row;
-} args;
+} thread_arg;
 
-void* matrix_thread(void* vargp)
+void* thread(void* vargp)
 {
-  args* arg;
-  size_t col, i;
+  thread_arg* args;
+  size_t row, col, i;
+  int** lop, **rop, **result;
   int dot_prod;
 
-  arg = (args*)vargp;
+  args = (thread_arg*)vargp;
+  lop = args->lop;
+  rop = args->rop;
+  result = args->result;
+  row = args->row;
 
   for (col = 0; col < M; col++)
   {
     dot_prod = 0;
-
     for (i = 0; i < M; i++)
-      dot_prod += arg->mat1[arg->row][i] * arg->mat2[i][col];
-
-    arg->dest[arg->row][col] = dot_prod;
+      dot_prod += lop[row][i] * rop[i][col];
+    result[row][col] = dot_prod;
   }
-  
 }
 
-int matrix_mult_t(const int mat1[N][M], const int mat2[N][M], int dest[N][M])
+void mmult_t(int** lop, int** rop, int** result)
 {
+  thread_arg args[N];
   pthread_t tids[N];
-  args arg_vector[N];
   size_t i;
-  
-  if (N != M)
-    return 0;
- 
-  memset(dest, 0, N*M*sizeof(int));
 
   for (i = 0; i < N; i++)
   {
-    arg_vector[i].mat1 = (const int**)mat1;
-    arg_vector[i].mat2 = (const int**)mat2;
-    arg_vector[i].dest = (int**)dest;
-    arg_vector[i].row = i;
-    pthread_create(&tids[i], NULL, matrix_thread, (void*)&arg_vector[i]);
+    args[i].lop = lop;
+    args[i].rop = rop;
+    args[i].result = result;
+    args[i].row = i;
+    pthread_create(&tids[i], NULL, thread, (void*)(&args[i]));
   }
 
   for (i = 0; i < N; i++)
     pthread_join(tids[i], NULL);
-
-  return 1;
 }
 
-void print_matrix(int mat[N][M])
+void print_matrix(int** mat)
 {
   size_t i, j;
-
   for (i = 0; i < N; i++)
   {
     for (j = 0; j < M; j++)
       printf("%d ", mat[i][j]);
     printf("\n");
   }
+  
 }
 
 int main(void)
 {
+  int** mat1, **mat2, **dest;
+  size_t i;
 
-  int mat1[][M] =
+  mat1 = malloc(N * sizeof(int*));
+  mat2 = malloc(N * sizeof(int*));
+  dest = malloc(N * sizeof(int*));
+
+  for (i = 0; i < N; i++)
   {
-    {1, -2},
-    {5, 12}
-  };
+    mat1[i] = malloc(M * sizeof(int));
+    mat2[i] = malloc(M * sizeof(int));
+    dest[i] = malloc(M * sizeof(int));
+  }
 
-  int mat2[][M] =
-  {
-    {0, 3},
-    {5, -1}
-  };
+  mat1[0][0] = 1;
+  mat1[0][1] = -2;
+  mat1[1][0] = 5;
+  mat1[1][1] = 12;
 
-  int dest[N][M];
-  
-  matrix_mult_t(mat1, mat2, dest);
+  mat2[0][0] = 0;
+  mat2[0][1] = 3;
+  mat2[1][0] = 5;
+  mat2[1][1] = -1;
+
+  mmult_t(mat1, mat2, dest);
+
+  puts("Left matrix:");
+  print_matrix(mat1);
+
+  puts("Right matrix:");
+  print_matrix(mat2);
+
+  puts("Result:");
   print_matrix(dest);
-  
+
+  for (i = 0; i < N; i++)
+  {
+    free(mat1[i]);
+    free(mat2[i]);
+    free(dest[i]);
+  }
+
+  free(mat1);
+  free(mat2);
+  free(dest);
+
   return 0;
 }
